87b9a1a0843e3af51b6a00b64b72e50e38377337
Doroteo Vidal <doroteo17vidal@hotmail.com>
2024-01-03 16:09:01 -0300

    se agrega servicios de signup con mensajes

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public user = {
+    username : '',
+    password : '',
+    name : '',
+    lastname : '',
+    email : '',
+    phone : ''  
   }
 
+  constructor(private userService : UserService, private snack : MatSnackBar) { }
+
+  ngOnInit(): void {}
+
+  formSubmit() {
+    console.log(this.user);
+    if(this.user.username == '' || this.user.username == null) {
+      this.snack.open("Username is required !!", "Accept", {
+        duration : 3000,
+        verticalPosition : 'top',
+        horizontalPosition : 'right'
+      });
+      return;
+    }
+
+    //Patron observable, para obtener datos, me tengo que suscribir (analogia YouTube)...
+    this.userService.addUser(this.user).subscribe(
+      (data) => {
+        console.log(data);
+        Swal.fire('User saved', 'User saved successfully', 'success');
+      }, (error) => {
+        console.log(error);
+        this.snack.open("An error has occurred in the system", "Accept", {
+          duration : 3000
+        });
+      }
+    )
+  }
 }
