2326f800eaa678a21c5ee188aad409727364c664
Doroteo Vidal <doroteo17vidal@hotmail.com>
2024-01-18 12:01:57 -0300

    se agrega metodo de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { LoginService } from 'src/app/services/login.service';
 
 @Component({
@@ -9,35 +10,52 @@ import { LoginService } from 'src/app/services/login.service';
 })
 export class LoginComponent implements OnInit {
 
+  isLoggingIn = false;
+
   loginData = {
     "email" : '',
     "password" : ''
   }
 
-  constructor(private snack:MatSnackBar, private loginService:LoginService) { }
+  constructor(private snack : MatSnackBar, private loginService : LoginService, private router : Router) { }
 
   ngOnInit(): void {}
 
   formSubmit() {
-    //console.log("The login button was clicked")
-    if(this.loginData.email.trim() == '' || this.loginData.email.trim() == null) {
-      this.snack.open("Email is required !!", "Accept", {
-        duration:3000
-      })
-      return;
-    }
-    if(this.loginData.password.trim() == '' || this.loginData.password.trim() == null) {
-      this.snack.open("Password is required !!", "Accept", {
-        duration:3000
-      })
-      return;
-    }
+    this.isLoggingIn = true;
 
     this.loginService.generateToken(this.loginData).subscribe(
       (data:any) => {
         console.log(data);
+        this.loginService.loginUser(data.token);
+        this.loginService.getCurrentUser().subscribe((user:any) =>{
+          this.loginService.setUser(user);
+          console.log(user);
+
+          if(this.loginService.getUserRole() == "ADMIN") {
+            //window.location.href = "/admin";
+            this.router.navigate(['admin']);
+            this.loginService.loginStatusSubject.next(true);
+          }else if(this.loginService.getUserRole() == "USER") {
+            //window.location.href = "/user-dashboard";
+            this.router.navigate(['user-dashboard']);
+            this.loginService.loginStatusSubject.next(true);
+          }else {
+            this.loginService.logout();
+          }
+        })
       }, (error) => {
         console.log(error);
+        if (error.status === 401) {
+          this.snack.open("Invalid email or password", "Accept", {
+            duration: 3000,
+          });
+        } else {
+          this.snack.open("An unexpected error occurred", "Accept", {
+            duration: 3000,
+          });
+        }
+        this.isLoggingIn = false;
       }
     )
   }
